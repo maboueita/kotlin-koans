05870c7fc59ca78d4dbdefd19e02f9201dd4adfe
maboueita <mahmoud.aboueita@gmail.com>
2017-06-04 15:10:51 +0200

    Task 04

--- src/i_introduction/_4_Lambdas/Lambdas.kt
+++ src/i_introduction/_4_Lambdas/Lambdas.kt
@@ -1,5 +1,6 @@
 package i_introduction._4_Lambdas
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import util.TODO
 import util.doc4
 
@@ -12,16 +13,17 @@ fun example() {
 }
 
 fun todoTask4(collection: Collection<Int>): Nothing = TODO(
-    """
+        """
         Task 4.
         Rewrite 'JavaCode4.task4()' in Kotlin using lambdas.
         You can find the appropriate function to call on 'collection' through IntelliJ IDEA's code completion feature.
         (Don't use the class 'Iterables').
     """,
-    documentation = doc4(),
-    references = { JavaCode4().task4(collection) })
+        documentation = doc4(),
+        references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+
+fun task4(collection: Collection<Int>): Boolean = collection.contains(collection.find { i -> i % 42 == 0 })
 
 
 
